3b81ccaa7e5492918aad4dd51beef0b6c9826674
Paweł Leśnik <pawel.lesnik@tdy.pl>
2023-02-27 22:17:57 +0100

    add save images

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useState, useEffect, useRef } from "react";
 
+
 function useWindowSize() {
   const [size, setSize] = useState([window.innerHeight, window.innerWidth]);
 
@@ -15,40 +16,7 @@ function useWindowSize() {
   return size;
 }
 
-const wrapText = function (ctx, text, x, y, maxWidth, lineHeight) {
-  // First, start by splitting all of our text into words, but splitting it into an array split by spaces
-  let words = text.split(" ");
-  let line = ""; // This will store the text of the current line
-  let testLine = ""; // This will store the text when we add a word, to test if it's too long
-  let lineArray = []; // This is an array of lines, which the function will return
 
-  // Lets iterate over each word
-  for (var n = 0; n < words.length; n++) {
-    // Create a test line, and measure it..
-    testLine += `${words[n]} `;
-    let metrics = ctx.measureText(testLine);
-    let testWidth = metrics.width;
-    // If the width of this test line is more than the max width
-    if (testWidth > maxWidth && n > 0) {
-      // Then the line is finished, push the current line into "lineArray"
-      lineArray.push([line, x, y]);
-      // Increase the line height, so a new line is started
-      y += lineHeight;
-      // Update line and test line to use this word as the first word on the next line
-      line = `${words[n]} `;
-      testLine = `${words[n]} `;
-    } else {
-      // If the test line is still less than the max width, then add the word to the current line
-      line += `${words[n]} `;
-    }
-    // If we never reach the full max width, then there is only one line.. so push it into the lineArray so we return something
-    if (n === words.length - 1) {
-      lineArray.push([line, x, y]);
-    }
-  }
-  // Return the line array
-  return lineArray;
-};
 
 function App() {
   const [randomImages, setRandomImages] = useState([]);
@@ -57,6 +25,15 @@ function App() {
   const [bottomText, setBottomText] = useState("");
   const [topText, setTopText] = useState("");
   const canvasRef = useRef(null);
+
+  const saveImageToLocal = (event) => {
+    let link = event.currentTarget;
+  
+    link.setAttribute('download', 'canvas.png');
+    let image = canvasRef.current.toDataURL('image/png');
+    link.setAttribute('href', image);
+    
+};
 
   const fetchImages = async () => {
     const req = await fetch("https://api.imgflip.com/get_memes");
@@ -68,35 +45,20 @@ function App() {
 
   useEffect(() => {
     const memImage = new Image();
+    memImage.crossOrigin = 'anonymous';
     memImage.src =
-      "https://images.unsplash.com/photo-1493612276216-ee3925520721?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1364&q=80";
-    memImage.onload = () => setImage(memImage);
+      "./assets/test.jpg"
+    memImage.onload = () => {
+      setImage(memImage);
+    };
+    console.log(memImage,'mem image')
   }, []);
 
-  // useEffect(() => {
-  //   if (canvasRef) {
-  //     const canvas = canvasRef.current;
-  //     const ctx = canvas.getContext("2d");
-  //     ctx.fillStyle = "blue";
-  //     ctx.fillRect(25, 300, 300, 50);
-  //     canvas.width = 300;
-  //     canvas.height = 300;
-  //   }
-
-  //   console.log(ctx);
-  //   const canvas = canvasRef.current;
-  //   const dpr = window.devicePixelRatio || 1;
-  //   let rect = canvas.getBoundingClientRect();
-  //   canvas.width = rect.width * dpr;
-  //   canvas.height = rect.height * dpr;
-  //   const ctx = canvas.getContext("2d");
-  //   if (image && canvas) {
-  //   }
-  // }, [image, canvasRef]);
+ 
 
   useEffect(() => {
     if (image) {
-      const size = window.innerWidth - 30;
+      const size = window.innerWidth >= 500 ? 470 : window.innerWidth - 30;
       const scale = window.devicePixelRatio;
       const canvas = canvasRef.current;
       const ctx = canvas.getContext("2d");
@@ -108,7 +70,8 @@ function App() {
       canvas.height = size;
       ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
       ctx.font = `${fontSize}px Comic Sans MS`;
-
+      ctx.shadowColor="black";
+      ctx.shadowBlur=7;
       ctx.fillStyle = "white";
       ctx.fillText(
         topText,
@@ -121,7 +84,6 @@ function App() {
         fontSize === 50 ? size - 70 : size - 50
       );
 
-      console.log(ctx.measureText(topText).width);
       // ctx.fillText(bottomText, 50, 50, 300);
       // wrapText(ctx, bottomText, 50, 50, 100, 16);
     }
@@ -135,6 +97,8 @@ function App() {
 
       <div className="images-container">
         <div className="canvas-wrapper">
+        <a id="download_image_link" href="download_link" onClick={saveImageToLocal}>Download Image</a>
+
           <input
             onChange={(e) => setTopText(e.target.value)}
             value={topText}
