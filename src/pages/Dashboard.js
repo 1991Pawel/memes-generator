3753a83fc63bf6fab4141a9d264b3bc2d450a7b0
Stoprocent <lesnik.pawel1991@gmail.com>
2023-03-09 14:21:39 +0100

    add wrap text option

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -106,6 +106,41 @@ function Dashboard() {
 
   // }
 
+  const wrapText = function (ctx, text, x, y, maxWidth, lineHeight) {
+    // First, start by splitting all of our text into words, but splitting it into an array split by spaces
+    let words = text.split(" ");
+    let line = ""; // This will store the text of the current line
+    let testLine = ""; // This will store the text when we add a word, to test if it's too long
+    let lineArray = []; // This is an array of lines, which the function will return
+
+    // Lets iterate over each word
+    for (var n = 0; n < words.length; n++) {
+      // Create a test line, and measure it..
+      testLine += `${words[n]} `;
+      let metrics = ctx.measureText(testLine);
+      let testWidth = metrics.width;
+      // If the width of this test line is more than the max width
+      if (testWidth > maxWidth && n > 0) {
+        // Then the line is finished, push the current line into "lineArray"
+        lineArray.push([line, x, y]);
+        // Increase the line height, so a new line is started
+        y += lineHeight;
+        // Update line and test line to use this word as the first word on the next line
+        line = `${words[n]} `;
+        testLine = `${words[n]} `;
+      } else {
+        // If the test line is still less than the max width, then add the word to the current line
+        line += `${words[n]} `;
+      }
+      // If we never reach the full max width, then there is only one line.. so push it into the lineArray so we return something
+      if (n === words.length - 1) {
+        lineArray.push([line, x, y]);
+      }
+    }
+    // Return the line array
+    return lineArray;
+  };
+
   const removeMem = async (item) => {
     const { error } = await supabase
       .from("mem")
@@ -144,6 +179,27 @@ function Dashboard() {
     };
   }, []);
 
+  function getLines(ctx, text, maxWidth) {
+    if (text) {
+      var words = text.split(" ");
+      var lines = [];
+      var currentLine = words[0];
+
+      for (var i = 1; i < words.length; i++) {
+        var word = words[i];
+        var width = ctx.measureText(currentLine + " " + word).width;
+        if (width < maxWidth) {
+          currentLine += " " + word;
+        } else {
+          lines.push(currentLine);
+          currentLine = word;
+        }
+      }
+      lines.push(currentLine);
+      return lines;
+    }
+  }
+
   useEffect(() => {
     if (image) {
       const size = window.innerWidth >= 500 ? 470 : window.innerWidth - 30;
@@ -161,16 +217,30 @@ function Dashboard() {
       ctx.shadowColor = "black";
       ctx.shadowBlur = 7;
       ctx.fillStyle = "white";
-      ctx.fillText(
+      let wrappedText = wrapText(
+        ctx,
         topText,
-        size / 2 - ctx.measureText(topText).width / 2,
-        fontSize === 50 ? canvas.height - size + 70 : canvas.height - size + 50
+        85,
+        size / 2 - ctx.measureText(bottomText).width + 150,
+        canvas.width - 100,
+        40
       );
-      ctx.fillText(
-        bottomText,
-        size / 2 - ctx.measureText(bottomText).width / 2,
-        fontSize === 50 ? size - 70 : size - 50
-      );
+      wrappedText.forEach(function (item) {
+        ctx.fillText(item[0], item[1], item[2]);
+      });
+      // ctx.fillText(
+      //   topText,
+      //   size / 2 - ctx.measureText(topText).width / 2,
+      //   fontSize === 50 ? canvas.height - size + 70 : canvas.height - size + 50
+      // );
+      // ctx.fillText(
+      //   bottomText,
+      //   size / 2 - ctx.measureText(bottomText).width / 2,
+      //   fontSize === 50 ? size - 70 : size - 50
+      // );
+
+      let x = getLines(ctx, topText, 500);
+      console.log(x);
 
       // ctx.fillText(bottomText, 50, 50, 300);
       // wrapText(ctx, bottomText, 50, 50, 100, 16);
