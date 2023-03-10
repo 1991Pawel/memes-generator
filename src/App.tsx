ca87ecc4a9969cf30590c3d49cab14a0fbf32791
Stoprocent <lesnik.pawel1991@gmail.com>
2023-03-10 15:42:00 +0100

    fix typesript template

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import styles from "./app.module.css";
 import { Input } from "./components/Input/Input";
+
 function App() {
   return (
     <div className="App">
-      APLIKACJA
-      <h2>LOREM</h2>
-      <Input label="Login" name="Login" />
-      <Input label="Hasło" name="hasło" />
+      <Input label="test" name="test" />
     </div>
   );
 }
--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,12 +1,17 @@
 import { InputHTMLAttributes } from "react";
 
+import s from "./Input.module.css";
+
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
   label: string;
 }
 export const Input = ({ name, label, ...rest }: InputProps) => {
+  {
+    // console.log(s);
+  }
   return (
-    <div className="input-wrapper">
+    <div className={s.wrapper}>
       <label htmlFor={name}>{label}</label>
       <input id={name}></input>
     </div>
