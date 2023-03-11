98c13c177f19791b03ad8ab192482229ae3ad313
Stoprocent <lesnik.pawel1991@gmail.com>
2023-03-11 12:19:45 +0100

    add margin to input

--- src/components/PasswordStrengthBar/PasswordStrengthBar.tsx
+++ src/components/PasswordStrengthBar/PasswordStrengthBar.tsx
@@ -2,7 +2,7 @@ import s from "./PasswordStrengthBar.module.css";
 import cx from "classnames";
 
 const passwordType = {
-  week: { message: "Słabe", color: "#E5E4E2", colorBar: 1 },
+  week: { message: "Słabe", color: "red", colorBar: 1 },
   medium: { message: "Średnie", color: "#FFC72C", colorBar: 3 },
   strong: { message: "Śilne", color: "#00AB66", colorBar: 4 },
 };
@@ -25,12 +25,6 @@ export const PasswordStrengthBar = ({ passwordValue }: any) => {
 
   const verifiedList = Object.values(strengthChecks).filter((value) => value);
 
-  const getActiveColor = (type: string) => {
-    if (type === passwordType.week.message) return passwordType.week.color;
-    if (type === passwordType.medium.message) return passwordType.medium.color;
-    return passwordType.strong.color;
-  };
-
   const getActiveStrength = () => {
     if (verifiedList.length == 5) return passwordType.strong;
     if (verifiedList.length >= 2) return passwordType.medium;
@@ -47,9 +41,7 @@ export const PasswordStrengthBar = ({ passwordValue }: any) => {
             className={s.bar}
             style={{
               background:
-                activeStrength.colorBar > i
-                  ? activeStrength.color
-                  : passwordType.week.color,
+                activeStrength.colorBar > i ? activeStrength.color : "",
             }}
             key={i}
           ></span>
