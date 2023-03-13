7ff6e4c7eb6f9ae3082bdc7f368d189d8211b550
Stoprocent <lesnik.pawel1991@gmail.com>
2023-03-13 18:34:47 +0100

    Add avatar

--- src/components/HeaderSignIn/HeaderSignIn.tsx
+++ src/components/HeaderSignIn/HeaderSignIn.tsx
@@ -1,12 +1,13 @@
 import s from "./HeaderSignIn.module.css";
 
 import { ButtonLogout } from "components/ButtonLogout/ButtonLogout";
+import { UserInfo } from "components/UserInfo/UserInfo";
 
 export const HeaderSignIn = () => {
   return (
     <header className={s.header}>
-      <p>Header</p>
       <div className={s.ctaGroup}>
+        <UserInfo />
         <ButtonLogout>Wyloguj</ButtonLogout>
       </div>
     </header>
---
+++ src/components/UserInfo/UserInfo.tsx
@@ -0,0 +1,8 @@
+import s from "./UserInfo.module.css";
+export const UserInfo = () => {
+  return (
+    <div className={s.user}>
+      <span className={s.name}>PL</span>
+    </div>
+  );
+};
