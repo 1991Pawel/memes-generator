01f946fe086bb9d2ee6787a21e38bc6500dacec2
Stoprocent <lesnik.pawel1991@gmail.com>
2023-03-13 13:14:49 +0100

    add env

--- src/components/PrivateRoutes/PrivateRoutes.tsx
+++ src/components/PrivateRoutes/PrivateRoutes.tsx
@@ -6,6 +6,6 @@ import {
   Navigate,
 } from "react-router-dom";
 export const PrivateRoutes = () => {
-  let auth = { token: true };
+  let auth = { token: false };
   return auth.token ? <Outlet /> : <Navigate to="/" />;
 };
