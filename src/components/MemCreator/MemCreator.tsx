413cadb1fac91b1667d42656bc432da5ce079f28
Stoprocent <lesnik.pawel1991@gmail.com>
2023-03-15 21:44:13 +0100

    add MemCreator

--- src/components/MemCreator/MemCreator.tsx
+++ src/components/MemCreator/MemCreator.tsx
@@ -1,3 +1,29 @@
+import s from "./MemCreator.module.css";
+import { useState } from "react";
+import { Input } from "components/Input/Input";
+import { Button } from "components/Button/Button";
+
 export const MemCreator = () => {
-  return <div className={s.creator}>mem</div>;
+  const [text, setText] = useState("");
+  const [image, setImage] = useState(
+    "http://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Neighbours_Siamese.jpg/640px-Neighbours_Siamese.jpg"
+  );
+  return (
+    <div className={s.creator}>
+      <div className={s.top}>
+        <label htmlFor="text">Tekst mema</label>
+        <input
+          onChange={(e) => setText(e.target.value)}
+          id="name"
+          name="text"
+          type="text"
+        />
+      </div>
+      <div className={s.inner}>
+        <p className={s.text}>{text}</p>
+        <img src={image} alt={text} />
+      </div>
+      <Button>Gotowe</Button>
+    </div>
+  );
 };
