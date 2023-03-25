402f1df71e95f5d47eab39825512a9e12913351e
Stoprocent <lesnik.pawel1991@gmail.com>
2023-03-25 15:06:14 +0100

    remove useless object form PrivateRoutes

--- src/components/PrivateRoutes/PrivateRoutes.tsx
+++ src/components/PrivateRoutes/PrivateRoutes.tsx
@@ -1,6 +1,6 @@
 import { getUserFromSession } from "../../utils/index";
 import { Outlet, Navigate } from "react-router-dom";
 export const PrivateRoutes = () => {
-  const auth = { token: getUserFromSession() };
-  return auth.token ? <Outlet /> : <Navigate to="/" />;
+  const user = getUserFromSession();
+  return user ? <Outlet /> : <Navigate to="/" />;
 };
